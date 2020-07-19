f394db053a13eadb22ff306f8b83ee5e0b0f9789
Ian Yin <yin.ian093@gmail.com>
2020-07-19 18:04:35 +1000

    add the iife file

---
+++ nodejs/IIFE.js
@@ -0,0 +1,21 @@
+const obj = {};
+
+(function (myObject) {
+  let startTime;
+
+  function appStart() {
+    startTime = new Date();
+    console.log('app started');
+  }
+
+  function timestamp() {
+    console.log(`${new Date() - startTime} ms passed`);
+  }
+  myObject.timestamp = timestamp;
+  myObject.appStart = appStart;
+})(obj);
+
+obj.appStart();
+obj.timestamp();
+
+console.log(obj);
