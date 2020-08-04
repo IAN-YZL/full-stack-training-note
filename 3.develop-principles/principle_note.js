9e0020e053d0af48d72af90543f0e9d6f2a4eb1c
Ian Yin <yin.ian093@gmail.com>
2020-08-04 19:24:53 +1000

    Create the development principle file

---
+++ 3.develop-principles/principle_note.js
@@ -0,0 +1,10 @@
+// Readable, Maintainable, Reusable
+
+// SOLID原则
+// S — Single responsibility principle  | 单一职责
+// O — Open/closed principle            | 开闭原则
+// L — Liskov substitution principle    | 里氏替换
+// I — Interface segregation principle  | 接口隔离
+// D - Dependency inversion principle    | 依赖注入
+
+
