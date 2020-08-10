3227548d18b1ca0f59e8862978e54e0eafc4a8bb
Ian Yin <yin.ian093@gmail.com>
2020-08-10 18:42:59 +1000

    add a footer

--- 4.react.js/my-app/src/App.js
+++ 4.react.js/my-app/src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 import Header from './component/Header';
+import Footer from './component/Footer';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <Footer />
     </div>
   );
 }
