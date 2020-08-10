51f9a815dad1c13d80f2e57c59e048a8717b5d07
Ian Yin <yin.ian093@gmail.com>
2020-08-10 18:37:20 +1000

    add a react program

---
+++ 4.react.js/my-app/src/component/Footer/Copyright.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Copyright = () => (
+    <div>
+        
+    </div>
+)
---
+++ 4.react.js/my-app/src/component/Header/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => (
+    <nav className="navbar">
+        <ul className="navbar__list">
+            <li>
+                <a href="#" className="navbar__link">Home</a>
+                <a href="#" className="navbar__link">Service</a>
+                <a href="#" className="navbar__link">Log In</a>
+            </li>
+        </ul>
+    </nav>
+);
+
+export default Navbar;
