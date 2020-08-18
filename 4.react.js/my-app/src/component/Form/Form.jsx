7ce9b4e02ad5a7ebbe340041bf686106eda0ee7b
Ian Yin <yin.ian093@gmail.com>
2020-08-18 13:13:06 +1000

    add some update

--- 4.react.js/my-app/src/component/Form/Form.jsx
+++ 4.react.js/my-app/src/component/Form/Form.jsx
@@ -3,19 +3,6 @@ import React from 'react';
 class Form extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            availableTime: ["9:00", "10:00", "11:00", "12:00", "13:00"]
-        };
-
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    getAvailableTime() {
-        return this.state.availableTime;
-    }
-
-    handleClick(event) {
-        event.preventDefault();
     }
 
     render() {
@@ -23,13 +10,7 @@ class Form extends React.Component {
 
         return (
             <form>
-                <select>
-                    {availableTime.map(time => (
-                        <option value={time}>{time}</option>
-                    ))}
-                </select>
-                <input type="submit" value="Confirm"/>
-            
+                
             </form>
         )
     }
