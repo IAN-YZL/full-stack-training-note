787f17fb1cdd5a6fae0c0d0496a29b6d05947e29
Ian Yin <yin.ian093@gmail.com>
2020-09-06 18:58:59 +1000

    add promise function note

--- 5.mongodb/promise.js
+++ 5.mongodb/promise.js
@@ -10,12 +10,15 @@ function asyncSum(a, b) {
 
         setTimeout(() => {
         console.log(a + b);
-        res(a + b);
+        if(Math.random() > 0.5) {
+            res(a + b);
+        }
+        rej("no!");
     }, Math.random() * 1000)
     })
 }
 
-function sumAll() {
+// function sumAll() {
     // asyncSum(1, 1, (result) => {
     //     asyncSum(1, result, (result) => {
     //         asyncSum(1, result, () => {})
@@ -23,11 +26,27 @@ function sumAll() {
     // });
     // asyncSum(1, 2);
     // asyncSum(1, 3);
-    asyncSum(1, 1).then(result => {
-        return asyncSum(1, result);
-    }).then(result => {
-        return asyncSum(1, result);
-    })
+    // asyncSum(1, 1).then(result => {
+    //     return asyncSum(1, result);
+    // }).then(result => {
+    //     return asyncSum(1, result);
+    // }).catch(error => {
+    //     console.log(error);
+    // })
+// }
+
+//async await
+async function sumAll() {
+    try {
+        let result = await asyncSum(1, 1);
+        result = await asyncSum(1, result);
+        result = await asyncSum(1, result);
+    } catch (error) {
+        console.log(error);
+    }
+    
 }
+
+
 
 sumAll();
