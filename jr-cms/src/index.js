4399e57a26f8fd02ae3ad2a56cf884d08bb61b3f
Ian Yin <yin.ian093@gmail.com>
2020-09-08 20:28:26 +1000

    add error handler middle ware

--- jr-cms/src/index.js
+++ jr-cms/src/index.js
@@ -1,14 +1,18 @@
 require('dotenv').config();
+require('express-async-errors');
 const express = require('express');
 
 const routes = require('./routes');
 const { connectToDB } = require('./utils/db');
+const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api', routes);
+
+app.use(errorHandler);
 
 connectToDB();
 app.listen(3000, () => {
